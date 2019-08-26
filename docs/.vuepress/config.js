d9b675a1398c467af182fdf1f47fd7f7d63735f7
Iggy <igoririanto@rocketmail.com>
2019-08-26 18:30:21 -0500

    trying to get to generate nav dynamically

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,40 @@
+const { lstatSync, readdirSync } = require("fs");
+const { join } = require("path");
+const ENTRY_DIR = "docs";
+
+const isDotFile = fileName => fileName[0] === ".";
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory() && !isDotFile(dirent.name))
+    .map(dirent => dirent.name);
+
+const dirList = getDirectories(ENTRY_DIR);
+
+const fileList = dirList.forEach(dir => {
+  const fileDirPath = join("docs", dir);
+  const files = readdirSync(fileDirPath);
+});
+
+// need to have it in form of
+/*
+
+{
+text: "javascript",
+items: [
+    {text: "title-of-article", link: "folderName/title-of-article},
+    {text: "title-of-article", link: "folderName/title-of-article}
+  ]
+}
+Note that title-of-article needs to be capitalized and get rid of (-) 
+ * */
+
+const itemList = () => {
+  return [
+    { text: "How to write a blog awesomely", link: "/blogs/blog1" },
+    { text: "How to read a book", link: "/blogs/blog2" }
+  ];
+};
 module.exports = {
   title: "Hello VuePress",
   description: "Blog v0.0.1",
@@ -7,10 +44,7 @@ module.exports = {
       { text: "About", link: "/about" },
       {
         text: "Blog",
-        items: [
-          { text: "Blog1NavLInk", link: "/blog/blog1" },
-          { text: "Blog2NavLInk", link: "/blog/blog2" }
-        ]
+        items: itemList()
       }
     ]
   }
---
+++ docs/javascript/interesting-codes.md
@@ -0,0 +1,5 @@
+# List of Interesting JS codes
+
+
+## Resources
+[dev.to - JS one liners that make me excited](https://dev.to/healeycodes/javascript-one-liners-that-make-me-excited-56aj)
